3e1635ddc9e32616014cccbe4ad9808de5cf6d45
Redwan Khan <>
2026-01-29 20:41:04 +0100

    Deploy portfolio build to GitHub Pages

--- src/components/ui/profile/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as AboutSection } from "./AboutSection";
-export { default as FormationSection } from "./FormationSection";
-export { default as ExperienceSection } from "./ExperienceSection";
-export { default as LanguesSection } from "./LanguesSection";
-export { default as PassionsSection } from "./PassionsSection";
-export { default as ProfileImage } from "./ProfileImage";
-export { default as InteractivePiano } from "./InteractivePiano";
-export * from "./constants";
-
